d329d13bbc9312f1e6455a218a554d4af6310dbe
Khaled Khalifa <khaled.khalifa@tmentors.com>
2024-10-25 00:44:02 +0300

    Added files via upload

---
+++ Assets/Scripts/GroundSpawner.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject groundTile;
+    [SerializeField] GameObject Player;
+
+    [SerializeField] GameObject normalTile;
+    [SerializeField] GameObject emptyTile;
+    [SerializeField] GameObject burningTile;
+    [SerializeField] GameObject suppliesTile;
+    [SerializeField] GameObject stickyTile;
+    [SerializeField] GameObject boostTile;
+    [SerializeField] GameObject obstacle;
+
+    GameObject instantiatedTile;
+    private Vector3 nextSpawnPoint;
+
+    private List<GameObject> activeTiles = new List<GameObject>();
+
+    void SpawnTile(int id)
+    {
+        nextSpawnPoint.x = groundTile.transform.position.x;
+
+        switch (id)
+        {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                instantiatedTile = Instantiate(emptyTile, nextSpawnPoint, Quaternion.identity);
+                break;
+            case 5:
+            case 6:
+            case 7:
+                instantiatedTile = Instantiate(burningTile, nextSpawnPoint, Quaternion.identity);
+                break;
+            case 8:
+            case 9:
+                instantiatedTile = Instantiate(suppliesTile, nextSpawnPoint, Quaternion.identity);
+                break;
+            case 11:
+            case 12:
+                instantiatedTile = Instantiate(stickyTile, nextSpawnPoint, Quaternion.identity);
+                break;
+            case 13:
+            case 14:
+                instantiatedTile = Instantiate(boostTile, nextSpawnPoint, Quaternion.identity);
+                break;
+            case 15:
+            case 16:
+            case 17:
+            case 18:
+                instantiatedTile = Instantiate(obstacle, nextSpawnPoint, Quaternion.identity);
+                break;
+            default:
+                instantiatedTile = Instantiate(normalTile, nextSpawnPoint, Quaternion.identity);
+                break;
+
+        }
+
+        activeTiles.Add(instantiatedTile);
+
+        // Update the next spawn point to the position of the child 'NextSpawnPoint'
+        if (instantiatedTile.transform.childCount > 0)
+        {
+            nextSpawnPoint = instantiatedTile.transform.GetChild(0).position;
+        }
+    }
+
+    private void Start()
+    {
+
+        for (int i = 0; i < 5; i++)
+        {
+            SpawnTile(0);
+        }
+        for(int i = 0; i < 15; i++)
+        {
+            int random_number = Random.Range(1, 32);
+            SpawnTile(random_number);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        int random_number = Random.Range(1, 32); 
+
+        if (activeTiles.Count > 0)
+        {
+            // Get the last tile
+            GameObject lastTile = activeTiles[0];
+            float tileEndZ = lastTile.transform.position.z + lastTile.transform.localScale.z;
+
+            // Check if the player's z position is greater than the tile's end position
+            if (Player.transform.position.z > tileEndZ + 2)
+            {
+                Destroy(activeTiles[0]); // Destroy the first tile that the player has passed
+                activeTiles.RemoveAt(0); // Remove it from the list
+
+                // Spawn a new tile at the end of the path
+                SpawnTile(random_number);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,174 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField][Tooltip("Forward speed of the object")] float forwardSpeed;
+
+    [SerializeField][Tooltip("Horizontal speed of the object")] float horizontalSpeed;
+
+    [SerializeField] float jumpForce = 0.1f;
+
+    [SerializeField] TMP_Text scoreText;
+
+    [SerializeField] TMP_Text speedText;
+
+    [SerializeField] TMP_Text fuelText;
+
+    int score;
+    int fuel;
+    float fuelTime;
+    float scoreTime;
+    string playerSpeed;
+
+    Rigidbody rb;
+
+    bool isGrounded = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("Start");
+        forwardSpeed = 10.0f;
+        horizontalSpeed = 15.0f;
+        fuel = 50;
+        fuelTime = 0;
+        score = 0;
+        scoreTime = 0;
+        rb = this.gameObject.GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        MovePlayer();
+        UpdateSpeed();
+        UpdateScore();
+        UpdateFuel();
+        
+    }
+    void MovePlayer()
+    {
+        ////////////////////// MOVEMENT RELATED FUNCTION //////////////////////
+
+        // Capturing input from the user
+        float x = Input.GetAxis("Horizontal");
+
+        // Initializing the sideways and forwards movement vectors
+        Vector3 horizontalMove = new Vector3(0, 0, 0);
+        Vector3 forwardMove = new Vector3(0, 0, forwardSpeed * Time.fixedDeltaTime);
+
+        // Limiting the player's movement so they don't fall left or right of the platform
+        if (x > 0 && rb.position.x < 8.4)
+        {
+            horizontalMove = transform.right * x * horizontalSpeed * Time.fixedDeltaTime;
+        }
+        else if (x < 0 && rb.position.x > -8.4)
+        {
+            horizontalMove = transform.right * x * horizontalSpeed * Time.fixedDeltaTime;
+        }
+        
+        // Initiating movement 
+        rb.MovePosition(rb.position + horizontalMove + forwardMove);
+
+
+        // If user pressed space, invoke Jump function
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+
+        }
+    }
+
+    void Jump()
+    {
+        ////////////////////// JUMP RELATED FUNCTION //////////////////////
+
+        // Check if the user is grounded, if yes, jump
+        if (isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce);
+            isGrounded = false;
+        }
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        ////////////////////// COLLISION RELATED FUNCTION //////////////////////
+
+        // Whenever the user collides with any object, consider them grounded
+        isGrounded = true;
+
+        // Writing the logic for every tile
+        if (collision.gameObject.CompareTag("BurningTile"))
+        {
+            fuel -= 10;
+        }
+        else if (collision.gameObject.CompareTag("SuppliesTile"))
+        {
+            fuel = 50;
+        }
+        else if (collision.gameObject.CompareTag("BoostTile"))
+        {
+            if(forwardSpeed == 10.0f)
+            {
+                forwardSpeed *= 2;
+            }
+        }
+        else if (collision.gameObject.CompareTag("StickyTile"))
+        {
+            if(forwardSpeed == 20.0f)
+            {
+                forwardSpeed /= 2;
+            }
+        }
+
+    }
+    private void UpdateScore()
+    {        
+        ////////////////////// SCORE RELATED FUNCTION //////////////////////
+
+        // Updating score by 1 each second
+        scoreTime += Time.deltaTime;
+        if (scoreTime >= 1.0)
+        {
+            scoreTime = 0;
+            score += 1;
+            scoreText.text = "Score: " + score.ToString();
+        }
+        
+    }
+    private void UpdateSpeed()
+    {
+        ////////////////////// SPEED RELATED FUNCTION //////////////////////
+
+        // Changing the speed text in the ui
+        if (forwardSpeed == 10.0f)
+        {
+            playerSpeed = "Normal";
+        }
+        else
+        {
+            playerSpeed = "Fast";
+        }
+        speedText.text = "Speed: " + playerSpeed;
+    }
+    private void UpdateFuel()
+    {
+        ////////////////////// FUEL RELATED FUNCTION //////////////////////
+
+        // Changing the fuel and fuel text in the ui
+        fuelTime += Time.deltaTime;
+        if(fuelTime >= 1)
+        {
+            fuelTime = 0;
+            fuel -= 1;
+        }
+        fuelText.text = "Fuel: " + fuel.ToString();
+    }
+}
