a89cef5f90d6668b177d79011c8dd1d3db4f9f29
Khaled Khalifa <khaled.khalifa@tmentors.com>
2024-10-27 00:24:47 +0300

    Added pause menu music

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -8,6 +8,13 @@ public class PauseMenu : MonoBehaviour
     [SerializeField] public GameObject pauseMenu;
     public static bool isPaused = false;
 
+    public AudioSource audioSource;
+    public AudioClip pauseAudio;
+    private AudioClip gameAudio;
+    private float timestamp; 
+
+
+
     void Start()
     {
         pauseMenu.SetActive(false);
@@ -18,25 +25,37 @@ public class PauseMenu : MonoBehaviour
         {
             if(isPaused) { 
                 ResumeGame();
+
             }
             else
             {
                 PauseGame();
+                
             }
         }
 
     }
     public void PauseGame()
     {
+        gameAudio = audioSource.clip;
+        timestamp = audioSource.time;
+        audioSource.Pause();
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
         isPaused = true;
+
+        audioSource.clip = pauseAudio;
+        audioSource.Play();
     }
     public void ResumeGame()
     {
+        audioSource.Stop();
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
+        audioSource.clip = gameAudio;
+        audioSource.time = timestamp;
+        audioSource.Play();
     }
     public void RestartGameFromPause()
     {
